e8c43508acbb8aa8d7b45cd6f4286648b2f59681
Adriana <adri.nunhez@gmail.com>
2019-08-12 15:29:28 -0400

    First commit

---
+++ app-old.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+        res.write('HOla Mundo');
+        res.end();
+
+    })
+    .listen(8080);
+
+console.log('Escuchando el puerto 8080');
